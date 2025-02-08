714d1ce5d66e6dfc09ceb5a0de7043d8f6a71646
Ruikang Tao <rico00121@outlook.com>
2025-02-08 16:42:21 +0100

    feat: integrate google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from 'next-themes'
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <GoogleAnalytics gaId="G-3NTZYX6697" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
